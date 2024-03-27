20bf5e364abf70974bdc28750462eb3f9d290c31
Joey Wang <zywang110428@163.com>
2024-03-27 10:56:51 +0800

    增加md5码token校验

--- service/login.go
+++ service/login.go
@@ -4,14 +4,28 @@ import (
 	"errors"
 	"my-project/dao"
 	"my-project/models"
+	"my-project/utils"
 )
 
 func Login(userName, passwd string) (*models.LoginRes, error) {
-
+	passwd = utils.Md5Crypt(passwd, "mszlu")
 	user := dao.GetUser(userName, passwd)
 	if user == nil {
 		return nil, errors.New("账号密码不正确")
 	}
-	var lr = &models.LoginRes{}
+	uid := user.Uid
+	//生成token jwt技术生成令牌
+	token, err := utils.Award(&uid)
+	if err != nil {
+		return nil, errors.New("token未能生成")
+	}
+	var userInfo models.UserInfo
+	userInfo.Uid = user.Uid
+	userInfo.UserName = user.UserName
+	userInfo.Avatar = user.Avatar
+	var lr = &models.LoginRes{
+		token,
+		userInfo,
+	}
 	return lr, nil
 }
